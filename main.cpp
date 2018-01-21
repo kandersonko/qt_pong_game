f544c13b18761bbfa1c209a16ca2527ce47b083d
Koffi Anderson Koffi <kandersonko@gmail.com>
2018-01-21 04:41:21 -0800

    game objects added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setBaseSize(800,800);
     w.show();
 
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsRectItem>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // create a game scene
+    QGraphicsScene *scene = new QGraphicsScene(this);
+
+    // add game objects
+    QGraphicsRectItem *player = new QGraphicsRectItem(0, 0, 10, 80);
+    player->setBrush(Qt::red);
+
+    QGraphicsRectItem *computer = new QGraphicsRectItem(100, 0, 10, 80);
+    computer->setBrush(Qt::blue);
+
+    QGraphicsEllipseItem *ball = new QGraphicsEllipseItem(10, 10, 10, 10);
+    ball->setBrush(Qt::green);
+
+    scene->addItem(player);
+    scene->addItem(computer);
+    scene->addItem(ball);
+
+
+    // create a game view
+    ui->gameView->setScene(scene);
 }
 
 MainWindow::~MainWindow()
